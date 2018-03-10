e952017a43cc840e3107ce2ac8567399e6a538b7
Joshua Sohn <joshuadsohn@gmail.com>
2018-03-10 00:00:34 -0500

    adds default player demo

--- src/App.js
+++ src/App.js
@@ -1,63 +1,144 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AudioPlayer from 'react-modular-audio-player';
+import React, { Component } from "react";
+import AudioPlayer from "react-modular-audio-player";
+import './index.css'
 
-
-let rearrangedPlayer = [
-  {
-    className: "tier-top",
-    style: {margin: "0.3rem"},
-    innerComponents: [
-      { 
-        type: "play",
-        style: {width: "fit-content"}
-      },
-      {
-        type: "rewind",
-        style: {width: "fit-content"}
-      },
-      {
-        type: "forward",
-        style: {width: "fit-content"}
-      },
-      {
-        type: "volume"
-      }
-    ]
-  },
-  {
-    className: "tier-bottom",
-    style: {margin: "0rem 0.3rem 0.3rem 0.3rem"},
-    innerComponents: [
-      {
-        type: "time",
-        style: {width: "fit-content"}
-      },
-      {
-        type: "seek"
-      }
-    ]
-  }
-]
-
+let audioFiles = [
+    {
+      src: "/heyJude.mp3",
+      title: "Hey Jude",
+      artist: "The Beatles"
+    },
+    {
+      src: "/uptownFunk.mp3",
+      title: "Uptown Funk ft. Bruno Mars",
+      artist: "Mark Ronson"
+    },
+    {
+      src: "/rollingInTheDeep.mp3",
+      title: "Rolling In The Deep",
+      artist: "Adele"
+    }
+  ],
+  rearrangedPlayer = [
+    {
+      className: "tier-top",
+      style: { margin: "0.3rem" },
+      innerComponents: [
+        {
+          type: "play",
+          style: { width: "fit-content" }
+        },
+        {
+          type: "rewind",
+          style: { width: "fit-content" }
+        },
+        {
+          type: "forward",
+          style: { width: "fit-content" }
+        },
+        {
+          type: "volume"
+        }
+      ]
+    },
+    {
+      className: "tier-bottom",
+      style: { margin: "0rem 0.3rem 0.3rem 0.3rem" },
+      innerComponents: [
+        {
+          type: "time",
+          style: { width: "fit-content" }
+        },
+        {
+          type: "seek"
+        }
+      ]
+    }
+  ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="demo-header">
+          <h1 className="demo-header-title">Examples of react-modular-audio-player</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AudioPlayer 
-          audioFiles={[{src: '/audio.mp3'}]}
-          hideName={true}
-          playerWidth="10rem"
-          rearrange={rearrangedPlayer}/>
+
+{/* DEMO 1 */}
+        <div className="demo-audio-player">
+          <div className="demo-title">
+            <h3> Default Player </h3>
+          </div>
+          <AudioPlayer 
+            audioFiles={audioFiles}/>
+          <div className="demo-code">
+            <h4> Code </h4>
+          <pre> {
+`
+let audioFiles = [
+  {
+    src: "/heyJude.mp3",
+    title: "Hey Jude",
+    artist: "The Beatles"
+  },
+  {
+    src: "/uptownFunk.mp3",
+    title: "Uptown Funk ft. Bruno Mars",
+    artist: "Mark Ronson"
+  },
+  {
+    src: "/rollingInTheDeep.mp3",
+    title: "Rolling In The Deep",
+    artist: "Adele"
+  }
+]
+
+//inside render()
+<AudioPlayer
+   audioFiles={audioFiles}
+/>`} </pre>
+
+{/* DEMO 2 */}
+          </div>
+        </div>
+
+        <div className="demo-audio-player">
+          <div className="demo-title">
+            <h3> Default Player </h3>
+          </div>
+          <AudioPlayer 
+            audioFiles={audioFiles}/>
+          <div className="demo-code">
+            <h4> Code </h4>
+          <pre> {
+`
+let audioFiles = [
+  {
+    src: "/heyJude.mp3",
+    title: "Hey Jude",
+    artist: "The Beatles"
+  },
+  {
+    src: "/uptownFunk.mp3",
+    title: "Uptown Funk ft. Bruno Mars",
+    artist: "Mark Ronson"
+  },
+  {
+    src: "/rollingInTheDeep.mp3",
+    title: "Rolling In The Deep",
+    artist: "Adele"
+  }
+]
+
+//inside render()
+<AudioPlayer
+   audioFiles={audioFiles}
+/>`} </pre>
+          </div>
+        </div>
+
+
+
       </div>
     );
   }
